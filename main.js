d2972bc7176b34dd99736d40d8e7ee7d2fb53d5f
antu <antusaha970@gmail.com>
2023-01-01 14:15:46 +0600

    initial commit

---
+++ main.js
@@ -0,0 +1,78 @@
+document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+
+function submitIssue(e) {
+  const getInputValue = id => document.getElementById(id).value;
+  const description = getInputValue('issueDescription');
+  const severity = getInputValue('issueSeverity');
+  const assignedTo = getInputValue('issueAssignedTo');
+  const id = Math.floor(Math.random()*100000000) + '';
+  const status = 'Open';
+  const isClosed = "normal";
+
+  const issue = { id, description, severity, assignedTo, status,isClosed };
+  let issues = [];
+  if (localStorage.getItem('issues')){
+    issues = JSON.parse(localStorage.getItem('issues'));
+  }
+  issues.push(issue);
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  document.getElementById('issueInputForm').reset();
+  fetchIssues();
+  e.preventDefault();
+}
+
+const closeIssue = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const currentIssue = issues.find(issue => issue.id == id);
+  currentIssue.status = 'Closed';
+  currentIssue.isClosed = "line-through";
+  localStorage.setItem('issues', JSON.stringify(issues));
+  fetchIssues();
+
+  // document.getElementById(`${id}`).querySelector('.title').style.textDecoration = "line-through";
+}
+
+const deleteIssue = id => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  // localStorage.removeItem('issues');
+  for (let i = 0; i < issues.length; i++) {
+    let obj = issues[i];
+    let objId = obj.id;
+    if(objId == id){
+      issues.splice(i,1);
+    }
+  }
+  // const remainingIssues = issues.filter( issues.id !== id );
+  // issues.splice(0,1);
+  localStorage.setItem('issues', JSON.stringify(issues));
+  document.getElementById(`${id}`).style.display = 'none';
+}
+
+// const deleteIssue = id => {
+//   // const issues = JSON.parse(localStorage.getItem('issues'));
+//   const issues = localStorage.getItem('issues');
+//   console.log(issues);
+//   const remainingIssues = issues.filter( issues.id !== id );
+//   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+// }
+
+const fetchIssues = () => {
+  const issues = JSON.parse(localStorage.getItem('issues'));
+  const issuesList = document.getElementById('issuesList');
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    const {id, description, severity, assignedTo, status,isClosed} = issues[i];
+
+    issuesList.innerHTML +=   `<div class="well" id=${id}>
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3 class ="title" style="text-decoration: ${isClosed};"> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+  }
+}
